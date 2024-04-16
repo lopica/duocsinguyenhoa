c813c95d3ecf8addd4a62b2eddea0bb96c276ddc
lopica <ducanhedison@gmail.com>
2024-04-16 15:07:03 +0700

    decor cta

--- src/components/Benefit.jsx
+++ src/components/Benefit.jsx
@@ -9,20 +9,32 @@ export default function Benefit() {
         <ul className="benefit__list grid grid--3">
           <li>
             <img className="benefit__icon" src={Safe} alt="safe icon" />
-            <p className="benefit__text">An tâm</p>
-            <small className="benefit__description">Đội ngũ tư vấn có kinh nghiệm và chuyên môn cao, sãn sàng hỗ trợ bạn</small>
           </li>
           <li>
             <img className="benefit__icon" src={Simple} alt="simple kit icon" />
-            <p className="benefit__text">Đơn giản</p>
-            <small className="benefit__description">
-              Chúng tôi hỗ trợ tư vấn bạn trên mọi nền tảng phổ biến hiện nay (facebook, zalo)
-            </small>
           </li>
           <li>
             <img className="benefit__icon" src={Piggy} alt="piggy save money" />
+          </li>
+          <li>
+            <p className="benefit__text">An tâm</p>
+            <small className="benefit__description">
+              Đội ngũ tư vấn có kinh nghiệm và chuyên môn cao, sãn sàng hỗ trợ
+              bạn
+            </small>
+          </li>
+          <li>
+            <p className="benefit__text">Đơn giản</p>
+            <small className="benefit__description">
+              Chúng tôi hỗ trợ tư vấn bạn trên mọi nền tảng phổ biến hiện nay
+              (facebook, zalo)
+            </small>
+          </li>
+          <li>
             <p className="benefit__text">Tiết kiệm</p>
-            <small className="benefit__description">Đảm bảo chi phí luôn hợp lý, tiết kiệm</small>
+            <small className="benefit__description">
+              Đảm bảo chi phí luôn hợp lý, tiết kiệm
+            </small>
           </li>
         </ul>
       </div>
